1eb73c13f1c29fa90ed10935971765d1abd3a6c1
farza <farzanrichu610@gmail.com>
2025-08-03 01:55:02 +0530

    replaced the code with freq resp sepcifications

--- Exp-03/freq_resposne.m
+++ Exp-03/freq_resposne.m
@@ -1,49 +1,23 @@
-figure;
+% Define common denominator
+d = conv(conv([1 1], [1 5]), [1 0]);  % Denominator doesn't change
 
-% Q1
-n1 = [10 0];
-d1 = conv(conv([1 2], [1 2]), [1 10]);
-sys1 = tf(n1, d1);
-subplot(3, 2, 1);
-nyquist(sys1);
-title('Q1');
+% --- System 1 ---
+n1 = 10;
+sys1 = tf(n1, d);
+figure;                      % Open new figure
+margin(sys1);                % Plot gain/phase margin for sys1
+%title('System 1: n = 10');
 
-% Q2
-n2 = 6;
-d2 = conv(conv([1 1], [1 2]), [1 0]);
-sys2 = tf(n2, d2);
-subplot(3, 2, 2);
-nyquist(sys2);
-title('Q2');
+% --- System 2 ---
+n2 = 29.999;
+sys2 = tf(n2, d);
+figure;                      % Open new figure
+margin(sys2);
+%title('System 2: n = 29.999');
 
-% Q3
+% --- System 3 ---
 n3 = 100;
-d3 = conv([1 10], [1 0]);
-sys3 = tf(n3, d3);
-subplot(3, 2, 3);
-nyquist(sys3);
-title('Q3');
-
-% Q4
-n4 = 8;
-d4 = conv(conv([1 0], [1 1]), [1 4]);
-sys4 = tf(n4, d4);
-subplot(3, 2, 4);
-nyquist(sys4);
-title('Q4');
-
-% Q5
-n5 = 2 * sqrt(3);
-d5 = conv([1 0], [1 1]);
-sys5 = tf(n5, d5);
-subplot(3, 2, 5);
-nyquist(sys5);
-title('Q5');
-
-% Q6
-n6 = conv([1 1], [0 10]);
-d6 = conv([1 2], [1 4]);
-sys6 = tf(n6, d6);
-subplot(3, 2, 6);
-nyquist(sys6);
-title('Q6');
+sys3 = tf(n3, d);
+figure;                      % Open new figure
+margin(sys3);
+%title('System 3: n = 100');
