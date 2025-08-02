ab8a824a70116aa6fcbedd89b0d686717b045c47
farza <farzanrichu610@gmail.com>
2025-08-03 01:54:06 +0530

    modified the title as questions

--- Exp-03/nyquist_resp.m
+++ Exp-03/nyquist_resp.m
@@ -6,7 +6,7 @@ d1 = conv(conv([1 2], [1 2]), [1 10]);
 sys1 = tf(n1, d1);
 subplot(3, 2, 1);
 nyquist(sys1);
-title('Q1');
+title('10s/((s+2)^2(s+10))');
 
 % Q2
 n2 = 6;
@@ -14,7 +14,7 @@ d2 = conv(conv([1 1], [1 2]), [1 0]);
 sys2 = tf(n2, d2);
 subplot(3, 2, 2);
 nyquist(sys2);
-title('Q2');
+title('6/s(1+s)(2+s)');
 
 % Q3
 n3 = 100;
@@ -22,7 +22,7 @@ d3 = conv([1 10], [1 0]);
 sys3 = tf(n3, d3);
 subplot(3, 2, 3);
 nyquist(sys3);
-title('Q3');
+title('100/s(s+10)');
 
 % Q4
 n4 = 8;
@@ -30,7 +30,7 @@ d4 = conv(conv([1 0], [1 1]), [1 4]);
 sys4 = tf(n4, d4);
 subplot(3, 2, 4);
 nyquist(sys4);
-title('Q4');
+title('8/s(1+s)(4+s)');
 
 % Q5
 n5 = 2 * sqrt(3);
@@ -38,7 +38,7 @@ d5 = conv([1 0], [1 1]);
 sys5 = tf(n5, d5);
 subplot(3, 2, 5);
 nyquist(sys5);
-title('Q5');
+title('(2*3^1/2)/s(s+1)');
 
 % Q6
 n6 = conv([1 1], [0 10]);
